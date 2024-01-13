a53c50bdeb2b90caec363a10bb1dcb86a1fc8b7b
uruta <waltergagate443@gmail.com>
2024-01-13 08:19:02 +0800

    Added Custom Cursor on Banner Ads

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const slides = document.querySelectorAll('.slides');
 let counter = 0; 
 const goNext = document.querySelector('.next-btn'); 
 const goPrev = document.querySelector('.prev-btn'); 
+
 slides.forEach( (slide, index) => {
     slide.style.left = `${index * 100}%`; 
 })
@@ -29,3 +30,44 @@ const slide = () => {
         slide.style.transform = `translateX(-${counter * 100}%)`; 
     })
 }
+
+// banner custom cursor
+const bannerCursor = document.querySelector('.bannerCursor'); 
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+gsap.set(bannerCursor,{opacity: 0, xPercent: -50, yPercent: -50}); 
+
+document.addEventListener('mousemove', (e) => {
+    gsap.to(bannerCursor, 0.5, {x: e.clientX, y: e.clientY});  
+}); 
+
+// console.log(mouseX); 
+
+
+const enter = (e, image) => {
+    gsap.to(bannerCursor, 0.5, {opacity: 1, x:e.clientX, y:e.clientY, ease: 'power2'}); 
+    bannerCursor.style.backgroundImage = `url('${image}')`; 
+}
+
+const leave = (e) => {
+    gsap.to(bannerCursor, 0.5, {opacity: 0, x:e.clientX, y:e.clientY, ease: 'power2'}); 
+}
+
+nextBtn.addEventListener('mouseenter', (e) => {
+    enter(e, 'imgs/arrows-right.png'); 
+})
+
+nextBtn.addEventListener('mouseleave', (e) => {
+    leave(e); 
+})
+
+prevBtn.addEventListener('mouseenter', (e) => {
+    enter(e, 'imgs/arrows-left.png'); 
+})
+
+prevBtn.addEventListener('mouseleave', (e) => {
+    leave(e); 
+})
+
+
